2dc247318e18fcd9eaf44dda697a256b4884b86a
Kevin <kevinholtkamp26@gmail.com>
2022-04-17 15:07:30 +0200

    Improvements

--- PSWinRAR.psm1
+++ PSWinRAR.psm1
@@ -1,7 +1,12 @@
 function Compress-WinRAR(){
+    [CmdletBinding()]
     param(
-    [String] $CompressPath,
-    [String] $DestinationPath,
+    [Parameter(Position = 0, Mandatory = $true)]
+    [String] $DirectoryToCompress,
+
+    [Parameter(Position = 1, Mandatory = $true)]
+    [String] $ArchivePath,
+
     [Switch] $Delete,
     [Switch] $SafeDelete,
     [Switch] $IgnoreEmptyDirectories,
@@ -21,77 +26,102 @@ function Compress-WinRAR(){
 
     if($Recursive){
         $Switches = "$Switches -r"
+        Write-Verbose "Setting parameter -r since switch Recursive is set"
     }
     if($Delete){
         $Switches = "$Switches -df"
+        Write-Verbose "Setting parameter -df since switch Delete is set"
     }
     if($SafeDelete){
         $Switches = "$Switches -dr"
+        Write-Verbose "Setting parameter -dr since switch SafeDelete is set"
     }
     if($IgnoreEmptyDirectories){
         $Switches = "$Switches -ed"
+        Write-Verbose "Setting parameter -ed since switch IgnoreEmptyDirectories is set"
     }
     if($ErrorLogFile){
         $Switches = "$Switches -ilog$ErrorLogFile"
+        Write-Verbose "Setting parameter -ilog$ErrorLogFile since parameter ErrorLogFile is set"
     }
     if($FileMask){
         $Switches = "$Switches -n$FileMask"
+        Write-Verbose "Setting parameter -n$FileMask since parameter FileMask is set"
     }
     if($Password){
         $Switches = "$Switches -p$Password"
+        Write-Verbose "Setting parameter -p$Password since parameter Password is set"
     }
     if($RecoveryPercentage){
         $Switches = "$Switches -rr$($RecoveryPercentage)p"
+        Write-Verbose "Setting parameter -rr$($RecoveryPercentage)p since parameter RecoveryPercentage is set"
     }
     if($SolidArchive){
         $Switches = "$Switches -s"
+        Write-Verbose "Setting parameter -s since switch SolidArchive is set"
     }
     if($TestData){
         $Switches = "$Switches -t"
+        Write-Verbose "Setting parameter -t since switch TestData is set"
     }
     if($Confirm){
         $Switches = "$Switches -y"
+        Write-Verbose "Setting parameter -y since switch Confirm is set"
     }
     if(!$FullPath){
         $Switches = "$Switches -ep"
+        Write-Verbose "Setting parameter -ep since switch FullPath is not set"
     }
 
     $Switches = "$Switches -m$CompressionLevel"
+    Write-Verbose "Setting parameter -m$CompressionLevel with value from parameter CompressionLevel"
     $Switches = "$Switches -md$DictionarySize"
+    Write-Verbose "Setting parameter -md$DictionarySize with value from parameter DictionarySize"
     $Switches = "$Switches -mt$Threads"
+    Write-Verbose "Setting parameter -mt$Threads with value from parameter Threads"
 
-    Start-Process -FilePath "C:\Program Files\WinRAR\Rar.exe" -ArgumentList "a -ep $Switches $DestinationPath $CompressPath"
+    Write-Verbose "Calling WinRAR via command line: \
+Start-Process -FilePath $(Get-WinRARPath -ErrorAction "Stop")\Rar.exe -ArgumentList a -ep $Switches $ArchivePath $DirectoryToCompress"
+    Start-Process -FilePath "$(Get-WinRARPath -ErrorAction "Stop")\Rar.exe" -ArgumentList "a -ep $Switches $ArchivePath $DirectoryToCompress"
 }
 
 function Expand-WinRAR(){
+    [CmdletBinding()]
     param(
-    [String] $TargetPath,
+    [Parameter(Position = 0, Mandatory = $true)]
+    [String] $TargetDirectory,
+
+    [Parameter(Position = 1, Mandatory = $true)]
     [String] $ArchivePath,
+
     [String] $Password,
     [Switch] $Confirm
     )
 
     if($Password){
         $Switches = "$Switches -p$Password"
+        Write-Verbose "Setting parameter -p$Password since parameter Password is set"
     }
-    if($Password){
+    if($Confirm){
         $Switches = "$Switches -y"
+        Write-Verbose "Setting parameter -y since switch Confirm is set"
     }
 
-    Start-Process -FilePath "C:\Program Files\WinRAR\UnRAR.exe" -ArgumentList "x $Switches $ArchivePath $TargetPath"
+    Write-Verbose "Calling WinRAR via command line: \
+Start-Process -FilePath $(Get-WinRARPath -ErrorAction "Stop")\UnRAR.exe -ArgumentList x $Switches $ArchivePath $TargetDirectory"
+    Start-Process -FilePath "$(Get-WinRARPath -ErrorAction "Stop")\UnRAR.exe" -ArgumentList "x $Switches $ArchivePath $TargetDirectory"
 }
 
-function Validate-WinRAR(){
+function Test-WinRAR(){
+    [CmdletBinding()]
     param(
-        [String] $ArchivePath,
-        [String] $Password
+    [Parameter(Position = 0, Mandatory = $true)]
+    [String] $ArchivePath,
+
+    [String] $Password
     )
 
-    if($Password){
-        $Switches = "$Switches -p$Password"
-    }
-
-    if((Start-Process -FilePath "C:\Program Files\WinRAR\Rar.exe" -ArgumentList "t $Switches $ArchivePath" -PassThru -Wait).ExitCode -eq 0){
+    if((Check-WinRAR -ArchivePath $ArchivePath -Password $Password) -eq 0){
         return $True
     }
     else{
@@ -99,15 +129,39 @@ function Validate-WinRAR(){
     }
 }
 
-function Test-WinRAR(){
+function Check-WinRAR(){
+    [CmdletBinding()]
     param(
-        [String] $ArchivePath,
-        [String] $Password
+    [Parameter(Position = 0, Mandatory = $true)]
+    [String] $ArchivePath,
+
+    [String] $Password
     )
 
     if($Password){
         $Switches = "$Switches -p$Password"
+        Write-Verbose "Setting parameter -p$Password since parameter Password is set"
     }
 
-    return (Start-Process -FilePath "C:\Program Files\WinRAR\Rar.exe" -ArgumentList "t $Switches $ArchivePath" -PassThru -Wait).ExitCode
+    Write-Verbose "Calling WinRAR via command line: \
+Start-Process -FilePath $(Get-WinRARPath -ErrorAction "Stop")\Rar.exe -ArgumentList t $Switches $ArchivePath -PassThru -Wait"
+    return (Start-Process -FilePath "$(Get-WinRARPath -ErrorAction "Stop")\Rar.exe" -ArgumentList "t $Switches $ArchivePath" -PassThru -Wait).ExitCode
+}
+
+function Get-WinRARPath(){
+    [CmdletBinding()]
+    param()
+
+    if(Test-Path "C:\Program Files\WinRAR\"){
+        return "C:\Program Files\WinRAR\"
+    }
+    if(Test-Path "C:\Program Files (x86)\WinRAR\"){
+        return "C:\Program Files (x86)\WinRAR\"
+    }
+    throw "WinRAR installation not found"
+}
+
+#Check for WinRAR directory on module install and abort installation when WinRAR is not installed
+if(!(Get-WinRARPath -ErrorAction SilentlyContinue)){
+    throw "WinRAR installation not found, aborting module installation"
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Powershell WinRAR (PSWinRAR)
 This is a simple Powershell module that wraps the WinRAR command line into Powershell cmdlets with easier to read parameter naming
+
+
+## How to Use
+PSWinRAR can be easily installed by using `Install-Module -Name PSWinRAR` inside of powershell, after which the functions are usable
+
+
+## Functions
+- `Compress-WinRAR` is used to compress the contents of the directory specified in `-DirectoryToCompress` into the .rar file specified in `-ArchivePath`
+- `Expand-WinRAR` is used to decompress the contents of the .rar file specified in `-ArchivePath` into the directory specified in `-TargetDirectory`
+- `Check-WinRAR` is used to check the contents of the .rar file specified in `-ArchivePath` and returns the exit code returned by the WinRAR command line tool
+- `Test-WinRAR` is a wrapper for `Check-WinRAR` which returns `$true` if `Check-WinRAR` returns `0`, and returns `$false` otherwise
