61385467a00b4a298c74299a36dcc9594c96adfe
Kevin <kevinholtkamp26@gmail.com>
2022-04-22 14:35:22 +0200

    Addition of Publish.ps1 script

---
+++ Publish.ps1
@@ -0,0 +1,12 @@
+New-Item "./Publishing/PSWinRAR" -ItemType Directory -Force
+
+Copy-Item ./PSWinRAR.psd1 ./Publishing/PSWinRAR/PSWinRAR.psd1
+Copy-Item ./PSWinRAR.psm1 ./Publishing/PSWinRAR/PSWinRAR.psm1
+
+Publish-Module -Path ./Publishing/PSWinRAR -NuGetApiKey (Read-Host "Please enter API Key:") -Repository "PSGallery" -ErrorAction "continue"
+
+[version]$version = (Import-PowerShellDataFile ./PSWinRAR.psd1).ModuleVersion
+[version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1)
+Update-ModuleManifest -Path ./PSWinRAR.psd1 -ModuleVersion $NewVersion
+
+Remove-Item "./Publishing" -Recurse -Force -Verbose
